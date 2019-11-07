6ef0aa2aaad27d3e0e56b352450ff4e3bbf6d63b
pcea03 <psaltercea@gmail.com>
2019-11-07 15:33:19 +0800

    first step

---
+++ include/person.php
@@ -0,0 +1,16 @@
+<?php
+
+class Person
+{
+	// properties and methods goes here
+	public $first = "Psalter";
+	public $last  = "Cea";
+	public $age = 24;
+
+	public function setName($first){
+		$this->first = $first;
+	}
+}
+
+
+?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php include 'include/person.php';?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+<body>
+<?php 
+$person1 = new Person();
+$person1->setName("Psalter");
+echo $person1->first;
+
+$person2 = new Person();
+$person2->setName("Angelo");
+echo $person2->first;
+
+?>
+</body>
+</html>
